4632c2184fae321bbe61b928ed0014070feb8299
空城依旧丶 <fsk950512>
2019-01-29 08:17:58 +0800

    fsk, 剧照

--- app/src/main/java/com/bw/movie/activity/activity/fragment/adapter/activity/MovieDetailsActivity.java
+++ app/src/main/java/com/bw/movie/activity/activity/fragment/adapter/activity/MovieDetailsActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,14 +19,17 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.bw.movie.R;
+import com.bw.movie.activity.activity.fragment.adapter.adapter.StillsAdapter;
 import com.bw.movie.activity.activity.fragment.adapter.adapter.VideoAdapter;
 import com.bw.movie.activity.activity.fragment.adapter.bean.DetailsMovieBean;
+import com.bw.movie.activity.activity.fragment.adapter.bean.StillsBean;
 import com.bw.movie.apis.Apis;
 import com.bw.movie.mvc.presenter.MyPresenter;
 import com.bw.movie.mvc.view.MyView;
 import com.bw.movie.utils.ToastUtil;
 import com.facebook.drawee.view.SimpleDraweeView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -74,8 +78,13 @@ public class MovieDetailsActivity extends AppCompatActivity implements MyView {
     private TextView mDetailsPpActorName;
     private String mStarring;
     private View mNoticePpXl;
+    private View mStillsPpXl;
     private RecyclerView mNoticeRv;
+    private RecyclerView mStillsRv;
     private List<DetailsMovieBean.ResultBean.ShortFilmListBean> mShortFilmList;
+    private List<String> mPosterList;
+    private StillsAdapter mStillsAdapter;
+    private List<String> mArrayList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -107,6 +116,7 @@ public class MovieDetailsActivity extends AppCompatActivity implements MyView {
                 initNotice();
                 break;
             case R.id.dy_details_stills://剧照
+                initStills();
                 break;
             case R.id.dy_details_review://影评
                 break;
@@ -117,6 +127,39 @@ public class MovieDetailsActivity extends AppCompatActivity implements MyView {
                 ToastUtil.showToast("敬请期待");
                 break;
         }
+    }
+    //点击剧照弹出popupwindow
+    private void initStills() {
+        View view = LayoutInflater.from(this).inflate(R.layout.stills_popup_view, null, false);
+        PopupWindow popupWindow = new PopupWindow(view, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT, true);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        popupWindow.setOutsideTouchable(true);
+        //设置显示位置,findViewById获取的是包含当前整个页面的view
+        popupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+        //点击空白处时，隐藏掉pop窗口
+        popupWindow.setFocusable(true);
+        popupWindow.setTouchable(true);
+        popupWindow.showAsDropDown(mDyDetailsPrediction, 0, 0);
+        initStillsPopup(view, popupWindow);
+    }
+
+    private void initStillsPopup(View view, final PopupWindow popupWindow) {
+        mStillsPpXl = view.findViewById(R.id.stills_pp_xl);
+        mStillsRv = view.findViewById(R.id.stills_pp_rv);
+        mStillsPpXl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+            }
+        });
+        initStillsData();
+
+    }
+
+    private void initStillsData() {
+        mStillsRv.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        mStillsAdapter = new StillsAdapter(this, mArrayList);
+        mStillsRv.setAdapter(mStillsAdapter);
     }
 
     //点击预告弹出popupwindow
@@ -144,10 +187,10 @@ public class MovieDetailsActivity extends AppCompatActivity implements MyView {
                 JZVideoPlayer.releaseAllVideos();
             }
         });
-        init();
+        initNoticeData();
     }
 
-    private void init() {
+    private void initNoticeData() {
         mNoticeRv.setLayoutManager(new LinearLayoutManager(this));
         mNoticeRv.setHasFixedSize(true);
         VideoAdapter adapter = new VideoAdapter(this, mShortFilmList);
@@ -196,6 +239,7 @@ public class MovieDetailsActivity extends AppCompatActivity implements MyView {
     @Override
     public void onMySuccess(Object data) {
         if (data instanceof DetailsMovieBean) {
+            mArrayList = new ArrayList<>();
             DetailsMovieBean detailsMovieBean = (DetailsMovieBean) data;
             if (detailsMovieBean.getStatus().equals("0000")) {
                 DetailsMovieBean.ResultBean result = detailsMovieBean.getResult();
@@ -210,7 +254,14 @@ public class MovieDetailsActivity extends AppCompatActivity implements MyView {
                 mSummary = result.getSummary();//简介
                 mMovieTypes = result.getMovieTypes();//类型
                 mStarring = result.getStarring();//演员名字
-                mShortFilmList = result.getShortFilmList();
+                mShortFilmList = result.getShortFilmList();//视频
+                mPosterList = result.getPosterList();//剧照
+                for (int i = 0; i < mPosterList.size(); i++) {
+                    String[] split = mPosterList.get(i).split(",");
+                    for (int j = 0; j < split.length; j++) {
+                        mArrayList.add(split[j]);
+                    }
+                }
             }
         }
     }
