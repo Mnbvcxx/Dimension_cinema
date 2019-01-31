54a04bd66b2988b06fddeee3102152c40dbd2442
lenovo <1461078880@qq.com>
2019-01-31 10:37:55 +0800

    第五次大汇和

---
+++ app/src/main/java/com/bw/movie/movie/fragment/cinemaActivity/bean/MoveSeatBean.java
@@ -0,0 +1,26 @@
+package com.bw.movie.movie.fragment.cinemaActivity.bean;
+
+/**
+ * @author: pengbo
+ * @date:2019/1/30 desc:
+ */
+public class MoveSeatBean {
+    int num;
+    int userId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
