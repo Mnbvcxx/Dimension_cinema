c16110899e20baa26d5bb74e0a9adbd3c21e4c81
空城依旧丶 <fsk950512>
2019-02-20 10:53:39 +0800

    fsk,修复登录弹框

--- app/src/main/java/com/bw/movie/guideView/fragment/FragmentFourth.java
+++ app/src/main/java/com/bw/movie/guideView/fragment/FragmentFourth.java
@@ -1,6 +1,8 @@
 package com.bw.movie.guideView.fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import com.bw.movie.R;
 import com.bw.movie.activity.activity.MainActivity;
 import com.bw.movie.utils.AlertDialogUntil;
+import com.bw.movie.utils.IntentUtils;
 
 /**
  * @author : FangShiKang
@@ -26,12 +29,17 @@ public class FragmentFourth extends Fragment implements View.OnClickListener {
      * 立即体验
      */
     private Button mGuideButton;
+    private SharedPreferences ps;
+    private boolean isFirst = false;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_guide_view_the_fourth, container, false);
         initView(view);
+        //设置SharedPreferences 判断 是否第一次登陆
+        ps = getActivity().getSharedPreferences("config", Context.MODE_PRIVATE);
+        isFirst = ps.getBoolean("isFirst", false);
         return view;
     }
 
@@ -46,8 +54,18 @@ public class FragmentFourth extends Fragment implements View.OnClickListener {
             default:
                 break;
             case R.id.guide_button:
-               Intent intent = new Intent(getActivity(), MainActivity.class);
-               startActivity(intent);
+                /**
+                 * 如果不是第一次登陆的话 跳过启动页到登录页面
+                 */
+                if (isFirst) {
+                    ps.edit().putBoolean("isFirst", true).commit();
+                    IntentUtils.getInstence().intent(getActivity(), MainActivity.class);
+                    getActivity().finish();
+                }else {
+                    ps.edit().putBoolean("isFirst", true).commit();
+                    IntentUtils.getInstence().intent(getActivity(), MainActivity.class);
+                    getActivity().finish();
+                }
                 break;
         }
     }
